44378e5c8290833d38e0eac67f89aa849b790623
Liudmila Astakhova <astakhol@gmail.com>
2024-07-05 22:35:30 +0300

    add hometask-4.10

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 )
 
+const USD_EUR = 0.93
+const USD_RUB = 88.0
+
 func main() {
-	const USD_EUR = 0.93
-	const USD_RUB = 88.0
-	EUR := 100.0
-	EUR_RUB := EUR / USD_EUR * USD_RUB
-	fmt.Print(EUR_RUB)
+	countMoney, USD, EUR := getUserInput()
+}
+
+func getUserInput() (float64, string, string) {
+	var countMoney float64
+	var USD string
+	var EUR string
+	fmt.Print("Введите количество валюты: ")
+	fmt.Scan(&countMoney)
+	fmt.Print("Укажите вид исходной валюты: ")
+	fmt.Scan(&EUR)
+	fmt.Print("Укажите вид целевой валюты: ")
+	fmt.Scan(&USD)
+	return countMoney, EUR, USD
+}
+
+func calculateMoney(countMoney, USD, EUR) (float64, string, string) {
 
 }
